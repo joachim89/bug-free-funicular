5ac2b1f890f5609b1f9135483552e76f7887b53c
Joachim Skjævestad <joachim@biketrial.no>
2019-04-26 09:33:38 +0200

    updates. workan

--- sketch.js
+++ sketch.js
@@ -2,45 +2,81 @@ let menuItem = 1;
 let img;
 let w;
 let h;
+let r2p =true;
 
 function preload(){
+	
 	img = loadImage("icon.png");
 }
 function setup(){
 	w = windowWidth;
 	h = windowHeight;
 	createCanvas(w, h);
-	//frameRate(15);
-	lines = loadStrings("highscore.txt");
-	console.log(lines);
-	
+	//frameRate(15);	
 
 }
 function draw(){
-	background(81,95,186);
+	background(75,89,180);
+
+	
 	if(menuItem == 1){
+		if(!mouseIsPressed){
+			r2p=true;
+		}
 		imageMode(CENTER);
-		image(img,w/2,h/2,192,192);
-		if(mousePressed){
+		image(img,w/2,h/2,100,100);
+		if(mouseIsPressed ){
+			console.log("menu 1 press");
+			r2p = false;
 			menuItem=2;
 		}
 	}
 	if(menuItem == 2){
+		if(!mouseIsPressed){
+			r2p=true;
+		}
 		fill(255);
 		textAlign(CENTER);
 		text("Velkommen til denne appen.\nHer kan du finne ut alt mulig.\nog spille spill...",w/2,h/2);
+		noStroke();
+		rect(0,h-(h/3),w,10);
 
-		rect(w/2,h-(h/3),w,10);
-
-		if(mousePressed){
+		if(mouseIsPressed && r2p){
 			menuItem = 3;
+			r2p = false;
 
 		}
 	}
 	if(menuItem == 3){
+		if(!mouseIsPressed){
+			r2p=true;
+		}
 		background(random(0,255));
 		text("THE END IS NEAR",w/2,h/2);
+		if(mouseIsPressed && r2p){
+			menuItem = 4;
+			r2p = false;
+
+		}
 	}
+	if(menuItem == 4){
+		if(!mouseIsPressed){
+			r2p=true;
+		}
+		background(0,255,0);
+		rectMode(CENTER);
+		fill(0,150,0);
+		rect(w/2,h/2,w-10,h-10);
+		fill(0,100,0);
+		rect(w/2,h/2,w-30,h-30);
+		text("this is the end.",w/2,h/2);
+		if(mouseIsPressed && r2p){
+			menuItem = 4;
+			r2p = false;
+
+		}
+	}
+
 
 }
 
